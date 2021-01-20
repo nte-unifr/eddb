3e24bcab0cd8375e081d36436502a1383297f110
Alrick <alrick.deillon@unifr.ch>
2021-01-20 15:55:08 +0100

    Playing around NuxtJS basics

---
+++ layouts/default.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    Catalog
+    <Nuxt />
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,6 @@
 <template>
-  <h1>Hello world!</h1>
+  <div>
+    <h1>Hello world!</h1>
+    <NuxtLink to="/fun">Fun page</NuxtLink>
+  </div>
 </template>
