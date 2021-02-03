7ab93ed16512996264ddc4d71d15603e122c1c32
alrick <alrick.deillon@unifr.ch>
2021-02-03 16:39:31 +0100

    Clean index

--- pages/index.vue
+++ pages/index.vue
@@ -1,49 +1,8 @@
 <template>
-  <div class="container pt-4">
-    <div class="level">
-      <div class="level-left">
-        <div class="level-item">
-          <img src="~/assets/images/unifr_eddb.png" width="110" />
-        </div>
-        <div class="level-item">
-          <div>
-            <h1 class="title">Callisto</h1>
-            <p>
-              Iconography of Play and Games
-            </p>
-          </div>
-        </div>
-      </div>
-      <div class="level-right">
-        <div class="level-item">
-          <a class="button is-light">About the project</a>
-        </div>
-        <div class="level-item">
-          <a class="button is-light">EDDB</a>
-        </div>
-      </div>
+  <section class="section">
+    <div class="container">
+      <h1>Home</h1>
+      <Feather />
     </div>
-    
-
-      <div class="field">
-        <div class="control">
-          <input class="input" type="text" placeholder="Input">
-        </div>
-      </div>
-
-      <div class="field">
-        <p class="control">
-          <span class="select">
-            <select>
-              <option>Select dropdown</option>
-            </select>
-          </span>
-        </p>
-      </div>
-
-      <div class="buttons">
-        <a class="button is-primary">Primary</a>
-        <a class="button is-link">Link</a>
-      </div>
-  </div>
+  </section>
 </template>
