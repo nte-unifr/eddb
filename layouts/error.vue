f6640f6a8b9a35a10be9be7a3345bccac9098fb1
alrick <alrick.deillon@unifr.ch>
2021-06-01 14:36:22 +0200

    Implement error page layout

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,8 +1,18 @@
 <template>
   <div>
-    <h1 v-if="error.statusCode === 404">Page not found</h1>
-    <h1 v-else>An error occurred</h1>
-    <NuxtLink to="/">Home page</NuxtLink>
+    <TheQuickAccess />
+    <TheNavbar />
+    <article class="container">
+      <section class="hero is-warning">
+        <div class="hero-body">
+          <p class="title">An error occurred</p>
+          <p class="subtitle" v-if="error.statusCode === 404">
+            404 : Page not found
+          </p>
+        </div>
+      </section>
+      <VBack />
+    </article>
   </div>
 </template>
 
@@ -12,3 +22,9 @@
     layout: 'blank'
   }
 </script>
+
+<style lang="scss" scoped>
+  .hero {
+    margin-top: 5rem;
+  }
+</style>
