298f020bf006ec17c92658d58d9d79b80925a588
alrick <alrick.deillon@unifr.ch>
2021-06-04 15:10:11 +0200

    Improve image of item view

--- components/Item/Image.vue
+++ components/Item/Image.vue
@@ -1,7 +1,17 @@
 <template>
-  <figure v-if="this.images.length > 0">
-    <img :src="imageUrl" alt="Placeholder image">
-  </figure>
+  <div>
+    <figure>
+      <img :src="image" alt="" />
+      <div>
+        <a :href="original" class="button is-primary is-small">
+          <span class="icon">
+            <VIcon icon="external-link" :width=15 :height=15 />
+          </span>
+          <span>Original</span>
+        </a>
+      </div>
+    </figure>
+  </div>
 </template>
 
 <script>
@@ -9,12 +19,15 @@ import { mapState } from "vuex"
 
 export default {
   computed: {
-    imageUrl ({ $config: { baseURL } }) {
-      const width = 1600
-      const height = 1200
+    image ({ $config: { baseURL } }) {
+      const width = 1000
+      const height = 1100
       const quality = 90
       const fit = 'contain'
       return `${baseURL}/assets/${this.images[0]}?w=${width}&h=${height}&q=${quality}&f=${fit}`
+    },
+    original ({ $config: { baseURL } }) {
+      return `${baseURL}/assets/${this.images[0]}`
     },
     ...mapState('item', {
       images: state => state.item.images
@@ -25,12 +38,11 @@ export default {
 
 <style lang="scss" scoped>
   figure {
-    text-align: center;
+    padding: 10px;
+    background: #fff;
     img {
-      max-width: 680px;
-      max-height: 440px;
-      padding: 10px;
-      background: #fff;
+      max-width: 500px;
+      max-height: 550px;
     }
   }
 </style>
