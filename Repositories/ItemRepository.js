c566ae85adbc5403a8fe3cb140a4661801119864
alrick <alrick.deillon@unifr.ch>
2021-06-07 15:10:43 +0200

    Get only published items

--- Repositories/ItemRepository.js
+++ Repositories/ItemRepository.js
@@ -1,11 +1,14 @@
 import _ from "lodash"
 import { queryPath } from "~/Repositories/Drones/PathDrone.js"
 
+const limit = '&limit=-1'
+const filters = '&status=published'
+
 export default ($http, config) => ({
   index() {
-    return $http.$get(`${config.item.resource}?fields=*,${queryPath(config.item.fields)}&limit=-1`)
+    return $http.$get(`${config.item.resource}?fields=*,${queryPath(config.item.fields)}${limit}${filters}`)
   },
   get(id) {
-    return $http.$get(`${config.item.resource}/${id}?fields=*,${queryPath(config.item.fields)}`)
+    return $http.$get(`${config.item.resource}/${id}?fields=*,${queryPath(config.item.fields)}${filters}`)
   }
 })
